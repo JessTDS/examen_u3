5d84a15c57293cbb3d9666d17c5d9904e4fd023c
JessTDS <143725020+JessTDS@users.noreply.github.com>
2026-04-20 21:03:27 -0600

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,11 @@
+// Importar las funciones necesarias de Firebase
 import { getApp, getApps, initializeApp } from "firebase/app";
+// getAuth -- da acceso a todo lo de autenticación
 import { getAuth } from "firebase/auth";
+
 import { firebaseConfig } from "./config";
 
+// Evita inicializar Firebase más de una vez
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
